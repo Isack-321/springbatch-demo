9342885b44d5d99327ca8ff0a805a98318bfe688
Isack <ituwei21@gmail.com>
2021-10-11 00:54:43 +0300

    api documentation

--- src/main/java/com/dashboard/ipl/ipldashboard/IpldashboardApplication.java
+++ src/main/java/com/dashboard/ipl/ipldashboard/IpldashboardApplication.java
@@ -3,7 +3,10 @@ package com.dashboard.ipl.ipldashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class IpldashboardApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/dashboard/ipl/ipldashboard/controller/TeamController.java
+++ src/main/java/com/dashboard/ipl/ipldashboard/controller/TeamController.java
@@ -7,9 +7,11 @@ import com.dashboard.ipl.ipldashboard.repository.MatchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class TeamController {
 
     @Autowired
