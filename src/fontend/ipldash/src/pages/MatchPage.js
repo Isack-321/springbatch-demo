e4e5f3db863b77bf0558cca318498cf98986754d
Isack <ituwei21@gmail.com>
2021-11-12 17:42:37 +0300

    added app pages

--- src/fontend/ipldash/src/pages/MatchPage.js
+++ src/fontend/ipldash/src/pages/MatchPage.js
@@ -1,10 +1,46 @@
+import { React, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { MatchDetailsCard } from '../components/MatchDetailsCard';
 
+import { YearSelector } from '../components/YearSelector';
+
+import './MatchPage.scss';
 
 const MatchPage = () => {
-    return ( <div classNmae="matchpage">
 
-        <h1>matchpage</h1>
-    </div> );
+
+    const [matches, setMatches] = useState([]);
+    const { teamName, year } = useParams();
+    useEffect(
+        () => {
+         const fetchMatches = async () => {
+            const response = await fetch(`${process.env.REACT_APP_API_ROOT_URL}/team/${teamName}/matches?year=${year}`);
+            const data = await response.json();
+            setMatches(data);
+
+         };
+         fetchMatches();
+            
+
+
+        }, [teamName, year]
+    );
+
+   
+    return (
+        <div className="MatchPage">
+            <div className="year-selector">
+                <h3> Select Year </h3>
+                <YearSelector teamName={teamName} />
+            </div>
+            <div>
+                <h1 className="page-heading">{teamName} matches in {year}</h1>
+                {
+                    matches.map(match => <MatchDetailsCard key={match.id} teamName={teamName} match={match} />)
+                }
+            </div>
+
+        </div>
+    );
 }
- 
 export default MatchPage;
